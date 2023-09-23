1b3a06f6b6c48873c70838e04a95b7f39e829848
Brandon <brandonbb99@gmail.com>
2023-09-23 17:03:37 +0800

    CTF2

--- routes/CTF.py
+++ routes/CTF.py
@@ -9,5 +9,6 @@ logger = logging.getLogger(__name__)
 
 @app.route("/payload_crackme", methods=['GET'])
 def payload_crackme():
+    #334-12321321321
     return send_file('payload_crackme', as_attachment=True)
 
