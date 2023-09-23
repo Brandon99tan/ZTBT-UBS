df647c1c5e1e1c11235a4c54b039c60cf69a3f17
Brandon <brandonbb99@gmail.com>
2023-09-23 21:59:58 +0800

    Parking Naming ocnvention

--- routes/__init__.py
+++ routes/__init__.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 import routes.square
 import routes.lazydeveloper
 import routes.digitalcolony
-import routes.airport
+# import routes.airport
 import routes.railway_builder
 import routes.CTF
 import routes.Chinese_wall
